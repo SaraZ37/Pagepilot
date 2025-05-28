0a81b7ad365cbdad1ffb76a95d98e5acba6d25fa
saraZ37 <saranet87@hotmail.com>
2025-05-29 00:55:58 +0200

    Initial commit - Spring Boot Library Backend

---
+++ src/main/java/com/Pagepilot/Pagepilot/PagepilotApplication.java
@@ -0,0 +1,13 @@
+package com.Pagepilot.Pagepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagepilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagepilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pagepilot/Pagepilot/PagepilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pagepilot.Pagepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagepilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

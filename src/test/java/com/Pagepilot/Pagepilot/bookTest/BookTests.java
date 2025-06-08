f193aa55a76b22bbe4d19a63e0a09f37c0f62345
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2025-06-08 13:08:42 +0200

    testGetAllBooks was created

--- src/test/java/com/Pagepilot/Pagepilot/bookTest/BookTests.java
+++ src/test/java/com/Pagepilot/Pagepilot/bookTest/BookTests.java
@@ -2,6 +2,7 @@ package com.Pagepilot.Pagepilot.bookTest;
 
 import com.Pagepilot.Pagepilot.book.Book;
 import com.Pagepilot.Pagepilot.book.BookService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,13 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 public class BookTests {
 
-    private final BookService bookService;
-
-    public BookTests(BookService bookService) {
-        this.bookService = bookService;
-    }
+    @Autowired
+    private BookService bookService;
 
     @Test
+    @Transactional
     public void testGetAllBooks() {
         List<Book> books = bookService.getAllBooks();
         assertNotNull(books, "List of books should not be null");

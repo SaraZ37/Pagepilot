d2cafd0e684a7f69710614f7dc43e5620abdbea0
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2025-06-09 12:50:43 +0200

    US-06
    As a user, I want to view my current and past borrowed books so I can track due dates and manage my loans

--- src/main/java/com/Pagepilot/Pagepilot/loan/LoanRepository.java
+++ src/main/java/com/Pagepilot/Pagepilot/loan/LoanRepository.java
@@ -1,9 +1,13 @@
 package com.Pagepilot.Pagepilot.loan;
 
 import com.Pagepilot.Pagepilot.book.Book;
+import com.Pagepilot.Pagepilot.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
+    List<Loan> findByUser(User user);
 }
